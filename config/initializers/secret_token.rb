84fabf9400744e6bea7b0144a6f6f4664f4b073d
Ben <benjaminwpeacock@gmail.com>
2014-06-24 02:31:09 +0000

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cyti2::Application.config.secret_key_base = '8ab945784b902d6570fa22f132e3e41d0b4873ad084b02cf6862d18220c59abb62389d3c4c187eda2598cbfd1ebb0933ea093a50998f8ace8a1d80d8ac1088ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cyti2::Application.config.session_store :cookie_store, key: '_cyti2_session'
