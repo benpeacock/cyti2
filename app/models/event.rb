0d5007a6ed67a795ede0126d8ee2b38d84b50aea
Ben <benjaminwpeacock@gmail.com>
2014-07-01 15:24:38 +0000

    Edit event form working, updating Parse db

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,12 +9,6 @@ class Event < ActiveRecord::Base
 
   Parse.init :application_id => ENV["PARSE_APP_ID"], :api_key => ENV["PARSE_KEY"]
 
-  # def initialize(address)
-  # 	self.id = id
-  #   self.address = address
-  #   # include other attributes if this works
-  # end
-
   def self.all
   	get("https://api.parse.com/1/classes/Event")
   end
@@ -25,15 +19,13 @@ class Event < ActiveRecord::Base
 
   def self.update(event_params)
     event = Parse::Query.new("Event").eq("objectId", "kknBHqfwUo").get.first
-    event["address"] = "More updated address10"
-    event["imageThumbUrl"] = "http://www.google.com"
-    result = event.save
-    puts result
+    event["address"] = event_params[:address]
+    event["endDate"] = event_params[:endDate]
+    event["imageThumbUrl"] = event_params[:imageThumbUrl]
+    event["moreInfo"] = event_params[:moreInfo]
+    event["shortDescription"] = event_params[:shortDescription]
+    event["title"] = event_params[:title]
+    event.save
   end
-
-  # def self.update(event_params)
-  #   @objectId = event_params["objectId"]
-  #   post("https://api.parse.com/1/classes/Event/#{@objectId}", :body => {:address => 'updated address'}.to_json, :headers => { 'Content-Type' => 'application/json' })
-  # end
 
 end 
